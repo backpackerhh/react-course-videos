0d82632debc011b70df7b9e0c6eb508094de9ae6
David Montesdeoca <david.montesdeoca@bluetab.net>
2020-01-20 09:48:29 +0100

    Allow to set state with search results after async requests

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,30 @@
 import React from "react";
 
+import youtube from "../api/youtube";
 import SearchBar from "./SearchBar";
 
 export default class App extends React.Component {
+  state = {
+    videos: []
+  };
+
   onSearchSubmit = async term => {
-    console.log(term);
+    const SEARCH_VIDEOS_ENDPOINT = "/search";
+
+    const response = await youtube.get(SEARCH_VIDEOS_ENDPOINT, {
+      params: {
+        q: term
+      }
+    });
+
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found {this.state.videos.length} videos
       </div>
     );
   }
